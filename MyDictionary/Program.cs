4756a4f0f4f3b091000544d3be08a2bbb8b4a02c
Ahmet Metin Bahçeci <ahmetmetinbahceci@gmail.com>
2021-02-03 22:39:17 +0300

    Proje Ödevim

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Felix Wankel'in Öz Geçmişi :");
+            Dictionary<int, string> dictonary = new Dictionary<int, string>();
+            dictonary.Add(1902, "13 Ağustos: Felix Wankel'in Doğumu Yılı");
+            dictonary.Add(1954, "Döner Pistonlu Motor ( Rotary-Wankel motoru) Felix Wankel tarafından geliştirilmiştir.");
+            dictonary.Add(1988, "9 Ekim: Felix Wankel'in Ölüm Yılı");
+
+            foreach (var item in dictonary)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
